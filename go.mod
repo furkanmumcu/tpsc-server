b352d57eb2c7fa605001d45e7cb09fa981069656
furkanmumcu <furkanmumcu2@gmail.com>
2019-08-16 15:10:26 +0300

    db added

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module restGin
 
-require github.com/gin-gonic/gin v1.4.0
+require (
+	github.com/gin-gonic/gin v1.4.0
+	github.com/lib/pq v1.2.0
+)
--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
+
+func dbFuncCreate(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		//createDb if not exist
+		if _, err := db.Exec("CREATE TABLE IF NOT EXISTS passanger (id varchar(255),name varchar(255),vehicle varchar(255), isOk varchar(255))"); err != nil {
+			c.String(http.StatusInternalServerError,
+				fmt.Sprintf("Error creating database table: %q", err))
+			return
+		}
+	}
+}
 
 func main() {
 	r := gin.Default()
@@ -20,6 +34,15 @@ func main() {
 		name := c.Param("name")
 		c.JSON(http.StatusOK, gin.H{"user": name})
 	})
+
+	if os.Getenv("DATABASE_URL") != "" {
+		db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+		if err != nil {
+			log.Fatalf("Error opening database: %q", err)
+		}
+
+		r.GET("/createDB", dbFuncCreate(db))
+	}
 
 	port := os.Getenv("PORT")
 
