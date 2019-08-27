291d6ec654cf407535ed0f38e99a968ffbbf883e
furkanmumcu <furkanmumcu2@gmail.com>
2019-08-27 11:21:54 +0300

    refactor

--- db/restFuncs.go
+++ db/restFuncs.go
@@ -1,9 +1,87 @@
 package db
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"restGin/model"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
 func dummy() {
 	fmt.Println("")
+}
+
+func CreateDB(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		//createDb if not exist
+		if _, err := db.Exec("CREATE TABLE IF NOT EXISTS passanger (id varchar(255),name varchar(255),vehicle varchar(255), isOk varchar(255), primary key(id))"); err != nil {
+			c.String(http.StatusInternalServerError,
+				fmt.Sprintf("Error creating database table: %q", err))
+			return
+		}
+	}
+}
+
+func CreatePassanger(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		name := c.Param("name")
+		vehicle := c.Param("vehicle")
+		isOk := "false"
+
+		fmt.Println(id + name + vehicle + isOk)
+
+		sqlStatement := "INSERT INTO passanger VALUES ($1, $2, $3, $4)"
+		if _, err := db.Exec(sqlStatement, id, name, vehicle, isOk); err != nil {
+			c.String(http.StatusInternalServerError,
+				fmt.Sprintf("Error creating database table: %q", err))
+			return
+		}
+		c.String(http.StatusOK, "success")
+	}
+}
+
+func GetPassanger(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+
+		sqlStatement := "SELECT * FROM passanger WHERE id = $1"
+
+		passanger := model.Passanger{}
+		row := db.QueryRow(sqlStatement, id)
+		err := row.Scan(&passanger.Id, &passanger.Name, &passanger.Vehicle, &passanger.IsOk)
+
+		if err != nil {
+			c.String(http.StatusInternalServerError,
+				fmt.Sprintf("Error %q", err))
+			return
+		}
+
+		jResult, err := json.Marshal(passanger)
+		if err != nil {
+			return
+		}
+
+		c.String(http.StatusOK, string(jResult))
+	}
+}
+
+func GetCount(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		sqlStatement := "SELECT COUNT(*) FROM passanger"
+		row := db.QueryRow(sqlStatement)
+		var count int
+		err := row.Scan(&count)
+		if err != nil {
+			c.String(http.StatusInternalServerError,
+				fmt.Sprintf("Error %q", err))
+			return
+		}
+		c.String(http.StatusOK, strconv.Itoa(count))
+	}
 }
---
+++ router/router.go
@@ -0,0 +1,38 @@
+package router
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"restGin/db"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Start() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "it works!!!")
+	})
+
+	if os.Getenv("DATABASE_URL") != "" {
+		dbase, err := db.OpenDB()
+		if err != nil {
+			log.Fatalf("Error opening database: %q", err)
+		}
+
+		r.GET("/createDB", db.CreateDB(dbase))
+		r.GET("/createPassanger/:id/:name/:vehicle", db.CreatePassanger(dbase))
+		r.GET("/getPassanger/:id/", db.GetPassanger(dbase))
+		r.GET("/getCount", db.GetCount(dbase))
+	}
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "9191"
+	}
+
+	r.Run(":" + port)
+
+}
